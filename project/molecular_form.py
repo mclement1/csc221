d58c64170b093c44e91841c9fa989b77ad04a16a
Marjory Clement <mcclement@comcast.net>
2016-04-15 00:12:20 +0000

    check point

--- project/molecular_form.py
+++ project/molecular_form.py
@@ -14,18 +14,36 @@ atomic_symbols = ('H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al',
 'Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg','Bh','Hs',
 'Mt','Ds','Rg','Cn',)
 
+atomic_symbols = [x.lower() for x in atomic_symbols]
+
+atomic_masses = (1.007944,4.0026022,6.9412,9.0121823,10.8117,12.01078,14.00672,15.99943,
+18.99840325,20.17976,22.989769282,24.30506,26.98153868,28.08553,
+30.9737622,32.0655,35.4532,39.9481,39.09831,40.0784,44.9559126,47.8671,
+50.94151,51.99616,54.9380455,55.8452,58.9331955,58.69344,63.5463,65.382,
+69.7231,72.641,74.921602,78.963,79.9041,83.7982,85.46783,87.621,88.905852,
+91.2242,92.906382,95.962,98,101.072,102.905502,106.421,107.86822,112.4118,
+114.8183,118.7107,121.7601,127.603,126.904473,131.2936,132.90545192,
+137.3277,138.905477,140.1161,140.907652,144.2423,145,150.362,151.9641,
+157.253,158.925352,162.5001,164.930322,167.2593,168.934212,173.0545,
+174.96681,178.492,180.947882,183.841,186.2071,190.233,192.2173,195.0849,
+196.9665694,200.592,204.38332,207.21,208.980401,209,210,222,223,226,227,
+232.038062,231.035882,238.028913,237,244,243,247,247,251,252,257,258,259,262,
+267,268,271,272,270,276,281,280,285,)
+
+table = dict(zip(atomic_symbols, atomic_masses)) 
+
 
 def get_elements():
     elements = str(input('Please list the elements of which \
 your molecule is composed as a comma-separated list. \
 [ex: for water, type "H,O"] Type "quit" to quit. '))
-    elements = elements.replace(' ','')
-    #elements = (elements.lower()).replace(' ','')
+    #elements = elements.replace(' ','')
+    elements = (elements.lower()).replace(' ','')
     return elements
 
 def check_elements(elements):
     while True:
-        if elements.lower() == 'quit':
+        if elements == 'quit':
             sys.exit()
 
         elif elements == '':
@@ -40,43 +58,78 @@ of the correct form. Please try again (Type 'quit' to quit.) "))
 
 def clean_elements(elements):
     elements_list = []
-    elements = str(elements).replace(' ','')
-    #elements = elements.lower()
+    #elements = str(elements).replace(' ','')
     elements = elements.split(',')
     for element in elements:
         elements_list.append(element)
-    print(elements_list)
+    #print(elements_list)
     return elements_list
 
 def check_legit(elements_list, atomic_symbols):
     for element in elements_list:
         #print('hooray')
-        if element in atomic_symbols:
-           print('hooray')
+        if element.lower() in atomic_symbols:
+            continue
+            print('hooray')
         #while element not in atomic_symbols:
 
         else:
             elements = input("I'm sorry, but {i} is not \
 a legitimate atomic symbol. Please re-enter your list of \
 atomic symbols ".format(i = element))
-            #check_legit(elements,atomic_symbols)
             elements = check_elements(elements)
             elements = clean_elements(elements)
             elements = check_legit(elements,atomic_symbols)
             break
-            #continue
-        #else:
-            #print(atomic_symbols.values)
-        #break
 
-#
+def get_numbers():
+    numbers = str(input('Please list the number of each type of element \
+elements of which your molecule is composed as a comma-separated list. \
+Please list the values in the same order that you listed the elements \
+and please only enter integers. [ex: for water, type "2,0"] \
+Type "quit" to quit. '))
+    numbers = numbers.replace(' ','')
+    #print(numbers)
+    return numbers
+
+
+def check_numbers(numbers):
+    try:
+        numbers = numbers.lower()
+        if numbers == 'quit':
+            sys.quit
+
+        elif numbers == '':
+            numbers = str(input("I'm sorry, but your input was not \
+of the correct form. Please try again (Type 'quit' to quit.) "))
+            numbers = numbers.replace(' ','')
+            check_numbers(numbers)
+    except:
+        return numbers
+        
+
+
+def clean_numbers(numbers):
+    numbers_list = []
+    numbers = numbers.replace(' ','')
+    numbers = numbers.split(',')
+    for number in numbers:
+        numbers_list.append(number)
+    return numbers_list
+
+
+
+
+
 def main():
-    #get_elements()
     elements = get_elements()
     elements = check_elements(elements)
     elements_list = clean_elements(elements)
     check_legit(elements_list, atomic_symbols)
-
+    numbers = get_numbers()
+    numbers = check_numbers(numbers)
+    numbers = clean_numbers(numbers)
+    #print(table)
 if __name__ == '__main__':
     main()
          
