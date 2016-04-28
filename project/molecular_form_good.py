d55d15ce95b400252cfb0effdc2c81b44767f7b6
Marjory Clement <mcclement@comcast.net>
2016-04-28 23:24:39 +0000

    more work

--- project/molecular_form_good.py
+++ project/molecular_form_good.py
@@ -77,7 +77,7 @@ def func_one():
         print('\n')
         print("I'm sorry, but you seem to have entered \
 a numerical value, invalid punctuation mark, or the empty string. \
-Please try again.")
+Please try again." + '\n')
         #print('\n')
         elements = get_elements()
         elements_stripped = rem_comm_spaces(elements)
@@ -98,20 +98,110 @@ def func_two(elements):
 
 def func_three(elements_formatted):
     elements = elements_formatted.split(',')
+    elements_list = []
     for element in elements:
         if element in atomic_symbols:
-            continue
+            elements_list.append(element)
+            #return elements_list
+
         else:
-            print('\n')
-            print("I'm sorry, but you have entered \
-a non-valid atomic symbol. Please try again.")
-            print('\n')
-            elements = func_one()
-            elements_formatted = func_two(elements)
             continue
-    return elements
+    return elements,elements_list
+    #return elements_list
+    #if len(elements) == len(elements_list):
+        #return True
+    #else:
+        #return False
+    #bool_value = all(i in atomic_symbols for i in elements)
+    #return bool_value
+    #if all( i in elements in atomic_symbols:
+        #return True
+    #else:
+        #return False
+    #for element in elements:
+        #if element in atomic_symbols:
+            #continue
+        #else:
+            #print("I'm sorry, but you have entered \
+    #a non-valid atomic symbol. Please try again." + '\n')
+            #print('\n')
+            #elements = func_one()
+            #elements_formatted = func_two(elements)
+            #break
+            #return elements_formatted
+            #legit_bool = func_three(elements_formatted)
+            #continue
+    #return elements
+    #legit_bool = lambda atomic_symbols,elements: all(x in atomic_symbols for x in elements)
+    #print(legit_bool)
+    #return legit_bool, elements
+
+#def func_three_half(legit_bool,elements):
+    #while legit_bool == False:
+        #print("I'm sorry, but you have entered \
+#a non-valid atomic symbol. Please try again." + '\n')
+        #print('\n')
+        #elements = func_one()
+        #elements_formatted = func_two(elements)
+        #legit_bool = func_three(elements_formatted)
+        #continue
+        #func_three_half(legit_bool,elements)
+        #return elements
+        #return legit_bool
+        #continue
+    #return elements
+    #for element in elements:
+    #while elements not in atomic_symbols:
+        #elements = func_one()
+        #elements_formatted = func_two(elements)
+        #elements = elements_formatted.split(',')
+        #return elements
+            #continue
+
+        #if element in atomic_symbols:
+            #return True
+            #continue
+        #else:
+            #print('\n')
+            #print("I'm sorry, but you have entered \
+#a non-valid atomic symbol. Please try again." + '\n')
+            #print('\n')
+            #elements = func_one()
+            #elements_formatted = func_two(elements)
+            #elements = elements_formatted.split(',')
+            #break
+            #return elements
+            #return elements_formatted
+            #continue
+    #return element
 
 
+def func_three_a(elements, elements_list):
+    bool_legit = len(elements) == len(elements_list)
+    return bool_legit
+
+
+def func_three_b(bool_legit):
+    while bool_legit == False:
+        print("I'm sorry, but you have entered an invalid \
+atomic symbol. Please try again." + '\n')
+        elements = func_one()
+        elements_formatted = func_two(elements)
+        elements, elements_list = func_three(elements_formatted)
+        bool_legit = func_three_a(elements,elements_list)
+
+    return elements_list
+
+
+
+
+
+#def func_three_a(bool_legit):
+    #if bool_legit == True:
+        #print('yay')
+
+    #else:
+        #pass
 
 
 def func_four():
@@ -124,11 +214,11 @@ def func_four():
         if numbers == 'quit':
             sys.exit()
         else:
-            print('\n')
+            #print('\n')
             print("I'm sorry, but you seem to have entered \
 a letter, an invalid punctuation mark, or the empty string. \
-Please try again.")
-            print('\n')
+Please try again." + '\n')
+            #print('\n')
             numbers = get_numbers()
             numbers_stripped = rem_comm_spaces(numbers)
             num_only = only_num(numbers_stripped)
@@ -153,10 +243,10 @@ def func_five(numbers):
 def func_six(elements_list, numbers_list):
     bool_value = comp_lists(elements_list, numbers_list)
     while bool_value == False:
-        print('\n')
+        #print('\n')
         print("I'm sorry, but you have not entered the \
-same number of integers as atomic symbols. Please try again.")
-        print('\n')
+same number of integers as atomic symbols. Please try again." + '\n')
+        #print('\n')
         elements_list = func_three(func_two(func_one()))
         numbers_list = func_five(func_four())
         return elements_list, numbers_list
@@ -181,10 +271,10 @@ def func_nine():
     calc_cleaned = clean_what_calc(calc)
     alpha_bool_value = is_alpha(calc_cleaned)
     while alpha_bool_value == False:
-        print('\n')
+        #print('\n')
         print("I'm sorry, but you have entered a number, \
-a punctuation mark, or the empty string. Please try again")
-        print('\n')
+a punctuation mark, or the empty string. Please try again" + '\n')
+        #print('\n')
         calc = what_calc()
         calc_cleaned = clean_what_calc(calc)
         alpha_bool_value = is_alpha(calc_cleaned)
@@ -201,10 +291,10 @@ def func_ten(calc_cleaned):
             sys.exit()
 
         else:
-            print('\n')
+            #print('\n')
             print("I'm sorry, but you have entered a non-legitimate \
-calculation type. Please try again.")
-            print('\n')
+calculation type. Please try again." + '\n')
+            #print('\n')
             calc_cleaned = func_nine()
             len_bool_value = check_len_what_calc(calc_cleaned)
             return calc_cleaned
@@ -226,14 +316,17 @@ def func_eleven(calc_cleaned, molar_mass):
 
             else:
                 print("I'm sorry, but you did not enter a floating-point \
-value. Please try again.")
+value. Please try again." + '\n')
                 moles = how_many_moles()
                 formatted_moles = format_moles(moles)
                 float_bool_value = confirm_float_moles(formatted_moles)
                 continue
         moles = make_moles_float(moles)
         grams = moles_to_grams(moles,molar_mass)
-        return str(grams) + ' ' + 'g'
+        answer = str(grams) + ' ' + 'g'
+        print('\n')
+        print("You have " + answer + '\n')
+        #return str(grams) + ' ' + 'g'
         #return moles
 
     elif calc == 'm':
@@ -247,63 +340,87 @@ value. Please try again.")
 
             else:
                 print("I'm sorry, but you did not enter a floating-point \
-value. Please try again.")
+value. Please try again." + '\n')
                 grams = how_many_grams()
                 formatted_grams= format_grams(grams)
                 float_bool_value = confirm_float_grams(formatted_grams)
                 continue
         grams = make_grams_float(grams)
         moles = grams_to_moles(grams,molar_mass)
-        return str(moles) + ' ' + 'mol'
+        answer = str(moles) + ' ' + 'm'
+        print('\n') 
+        print("You have " + answer + '\n')
+        #return str(moles) + ' ' + 'mol'
         #return grams
 
-
 def func_twelve():
-    print('\n')
+    #print('\n')
     another_calc_dec = another_calc()
     cleaned_calc_dec  = rem_comm_spaces(another_calc_dec)
-    alpha_bool_value = is_alpha(cleaned__calc_dec)
+    alpha_bool_value = is_alpha(cleaned_calc_dec)
     while alpha_bool_value == False:
-        print('\n')
+        #print('\n')
         print("I'm sorry, but I did not understand that. \
-Please try again. ")
-        print('\n')
+Please try again. " + '\n')
+        #print('\n')
         another_calc_dec = another_calc()
         cleaned_calc_dec = rem_comm_spaces(another_calc_dec)
         alpha_bool_value = is_alpha(cleaned_calc_dec)
         continue
     return another_calc_dec
+    
+    
+
+
+def func_thirteen(another_calc_dec,molar_mass):
     form_another_calc = format_input(another_calc_dec)
+    while form_another_calc not in ('quit','yes'):
+        #print('\n')
+        print("I'm sorry, but I did not understand that. \
+Please try again. " + '\n')
+        another_calc_dec = func_twelve()
+        form_another_calc = format_input(another_calc_dec)
+        continue
+
     if form_another_calc == 'quit':
         sys.exit()
-    elif form_another_calc == 'yes':
-        continue
-    else:
-        print('\n')
-        print("I'm sorry, but I did not understand that. \
-Please try again. ")
-        print('\n')
-        another_calc_dec = another_calc()
-        cleaned_calc_dec = rem_comm_spaces(another_calc_dec)
-        alpha_bool_value = is_alpha(cleaned_calc_dec)
-        return alpha_bool_value
-        continue
     
+    else:
+       calc_cleaned = func_nine()
+       calc_cleaned = func_ten(calc_cleaned)
+       answer = func_eleven(calc_cleaned, molar_mass)
+
+    another_calc_dec = func_twelve()
+    func_thirteen(another_calc_dec,molar_mass)
+    #form_another_calc = format_input(another_calc_dec)
+    #continue
+
+
+
+
 
 def main():
     elements = func_one()
     elements_formatted = func_two(elements)
-    elements_list = func_three(elements_formatted)
+    elements,elements_list = func_three(elements_formatted)
+    #bool_legit = func_three(elements_formatted)
+    bool_legit = func_three_a(elements,elements_list)
+    elements_list = func_three_b(bool_legit)
+    #elements_list = func_three(elements_formatted)
+    #legit_bool,elements = func_three(elements_formatted,atomic_symbols)
+    #func_three_half(legit_bool,elements)
     numbers = func_four()
     numbers_list = func_five(numbers)
     elements_list, numbers_list = func_six(elements_list, numbers_list)
     elements_dict = func_seven(elements_list, numbers_list)
     molar_mass = func_eight(elements_dict, data_table)
     calc_cleaned = func_nine()
+    calc_cleaned = func_ten(calc_cleaned)
     answer = func_eleven(calc_cleaned,molar_mass)
-    print('\n')
-    print(answer)
-    func_twelve()
+    another_calc_dec = func_twelve()
+    func_thirteen(another_calc_dec,molar_mass)
+    
+
 
 
 if __name__=='__main__':
