b96a203208de33acfe622cfb16f5c3eeccdf800c
Marjory Clement <mcclement@comcast.net>
2016-04-29 03:15:53 +0000

    final versions

---
+++ project/base_functions.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python3.4
+
+import sys
+
+#Tuple containing all valid atomic symbols
+
+atomic_symbols = ('H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al',
+'Si','P','S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu',
+'Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh',
+'Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd','Pm',
+'Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re','Os','Ir',
+'Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th','Pa','U','Np',
+'Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg','Bh','Hs',
+'Mt','Ds','Rg','Cn',)
+
+
+#Convert all atomic symbols to lowercase
+
+atomic_symbols = [x.lower() for x in atomic_symbols]
+
+
+#Tuple containing the atomic masses corresponding to the
+#atomic symbols
+
+atomic_masses = (1.007944,4.0026022,6.9412,9.0121823,10.8117,12.01078,14.00672,15.99943,
+18.99840325,20.17976,22.989769282,24.30506,26.98153868,28.08553,
+30.9737622,32.0655,35.4532,39.9481,39.09831,40.0784,44.9559126,47.8671,
+50.94151,51.99616,54.9380455,55.8452,58.9331955,58.69344,63.5463,65.382,
+69.7231,72.641,74.921602,78.963,79.9041,83.7982,85.46783,87.621,88.905852,
+91.2242,92.906382,95.962,98,101.072,102.905502,106.421,107.86822,112.4118,
+114.8183,118.7107,121.7601,127.603,126.904473,131.2936,132.90545192,
+137.3277,138.905477,140.1161,140.907652,144.2423,145,150.362,151.9641,
+157.253,158.925352,162.5001,164.930322,167.2593,168.934212,173.0545,
+174.96681,178.492,180.947882,183.841,186.2071,190.233,192.2173,195.0849,
+196.9665694,200.592,204.38332,207.21,208.980401,209,210,222,223,226,227,
+232.038062,231.035882,238.028913,237,244,243,247,247,251,252,257,258,259,262,
+267,268,271,272,270,276,281,280,285,)
+
+
+#Create a dictionary linking each atomic symbol
+#with the corresponding mass
+
+data_table = dict(zip(atomic_symbols, atomic_masses))
+
+
+#Collect a list of atomic symbols from user
+
+def get_elements():
+    elements = str(input('Please list the elements of which \
+your molecule is composed as a comma-separated list. \
+[ex: for water, type "H,O"] Type "quit" to quit. ' + '\n'))
+    return elements
+
+#Strip commas and spaces from input to determine
+#if any numeric or other special characters were entered
+
+def rem_comm_spaces(elements):
+    elements_stripped = elements.replace(' ','')
+    elements_stripped = elements_stripped.replace(',','')
+    return elements_stripped
+
+
+#Confirm that user entered only letters and not numbers
+#or other special characters
+
+def only_alpha(elements_stripped):
+    alpha_only = elements_stripped.isalpha()
+    return alpha_only
+
+
+#Convert user input to lowercase and remove
+#excess spaces
+
+def format_input(elements):
+    elements = (elements.lower()).replace(' ','')
+    return elements
+
+
+#Collect a list of numerical values from user
+
+def get_numbers():
+    numbers = str(input('Please list the number of each type of element \
+elements of which your molecule is composed as a comma-separated list. \
+Please list the values in the same order that you listed the elements \
+and please only enter integers. [ex: for water, type "2,1"] \
+Type "quit" to quit. ' + '\n'))
+    return numbers
+
+#Confirm that the user enter only integers
+
+def only_num(numbers):
+    num_only = numbers.isdigit()
+    return num_only
+
+
+#Confirm that the user entered the same number
+#of atomic symbols as numbers
+
+def comp_lists(elements_list, numbers_list):
+    if len(elements_list) == len(numbers_list):
+        return True
+    else:
+        return False
+
+
+#Combine elements_list and numbers_list together
+#into a dictionary called 'elements_dict'
+
+def zip_lists(elements_list, numbers_list):
+    elements_dict = dict(zip(elements_list, numbers_list))
+    return elements_dict
+
+
+#Collect input from the user concerning the kind
+#of calculation he would like to perform
+
+def what_calc():
+    calc = str(input("Would you like to convert to grams or to moles? \
+Enter 'g' for grams and 'm' for moles. Type 'quit' to quit." + '\n'))
+    return calc
+
+
+#Strip excess spaces from the user's input
+#and convert the input to lowercase
+
+def clean_what_calc(calc):
+    calc = calc.replace(' ','')
+    return calc
+
+
+#Determine if input is numerical or not
+
+def is_alpha(calc):
+    return calc.isalpha()
+
+
+#Check to see that the input is one-character
+#in length
+
+def check_len_what_calc(calc):
+    if len(calc) == 1:
+        return True
+    else:
+        return False
+
+
+#Confirm that the user input is one of the
+#two valid options
+
+def check_what_calc(calc):
+    if calc == 'g' or calc == 'm':
+        return True
+    else:
+        return False
+
+
+#Determine which calculation the user would
+#actually like to perform
+
+def analyze_what_calc(calc):
+    if calc == 'g':
+        return 'g'
+    elif calc == 'm':
+        return 'm'
+
+
+#Collect user input regarding his
+#starting number of moles
+
+def how_many_moles():
+    moles = str(input('How many moles \
+do you have? Type "quit" to quit. ' + '\n'))
+    return moles
+
+
+#Remove excess spaces from number of moles
+
+def format_moles(moles):
+    moles = moles.replace(' ','')
+    return moles
+
+
+#Determine whether or not a floating point
+#number was entered
+
+def confirm_float_moles(moles):
+    try:
+        moles = float(moles)
+        return True
+    except:
+        return False
+
+
+#Convert entered number of moles
+#to floating point number
+
+def make_moles_float(moles):
+    moles = float(moles)
+    return moles
+
+
+#Collect user input regarding his
+#starting number of grams
+
+def how_many_grams():
+    grams = str(input('How many grams \
+do you have? Type "quit" to quit. ' + '\n'))
+    return grams
+
+
+#Remove excess spaces from number of grams
+
+def format_grams(grams):
+    grams = grams.replace(' ','')
+    return grams
+
+
+#Determine whether or not a floating point
+#number was entered
+def confirm_float_grams(grams):
+    try:
+        grams = float(grams)
+        return True
+    except:
+        return False
+
+
+#Convert entered number of grams
+#to floating point number
+
+def make_grams_float(grams):
+    grams = float(grams)
+    return grams
+
+
+#Handle cases where a floating point number
+#was not entered
+
+def handle_not_float(starting):
+    starting = starting.lower()
+    if starting == 'quit':
+        return True
+    else:
+        return False
+
+
+#Calculate the molar mass of the molecule
+#in question
+
+def calc_molar_mass(elements_dict,data_table):
+    molar_mass = 0
+    for element in elements_dict.keys():
+        atomic_mass = float(data_table[element])
+        coefficient = float(elements_dict[element])
+        composite_mass = coefficient*atomic_mass
+        molar_mass = molar_mass + composite_mass
+    return molar_mass
+
+
+#Convert from moles to grams
+
+def moles_to_grams(moles,molar_mass):
+    grams = moles*molar_mass
+    return grams
+
+
+#Convert from grams to moles
+
+def grams_to_moles(grams, molar_mass):
+    moles = grams/molar_mass
+    return moles
+
+
+#Asks the user if he would like to perform another
+#calculation
+def another_calc():
+    another_calc_dec = str(input("Would you like to perform another \
+calculation on this same molecule? If so, type 'yes'; \
+otherwise, type 'quit' to quit. " + '\n'))
+    return another_calc_dec
+
+
---
+++ project/molar_conversion.py
@@ -0,0 +1,414 @@
+#!/usr/bin/env python3.4
+
+import sys
+
+from base_functions import (
+        get_elements,
+        rem_comm_spaces,
+        only_alpha,
+        format_input,
+        get_numbers,
+        only_num,
+        comp_lists,
+        zip_lists,
+        calc_molar_mass,
+        what_calc,
+        clean_what_calc,
+        is_alpha,
+        check_len_what_calc,
+        analyze_what_calc,
+        how_many_moles,
+        format_moles,
+        confirm_float_moles,
+        make_moles_float,
+        how_many_grams,
+        format_grams,
+        confirm_float_grams,
+        make_grams_float,
+        handle_not_float,
+        moles_to_grams,
+        grams_to_moles,
+        another_calc,
+)
+
+#Tuple containing all valid atomic symbols
+
+atomic_symbols = ('H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al',
+'Si','P','S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu',
+'Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc','Ru','Rh',
+'Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe','Cs','Ba','La','Ce','Pr','Nd','Pm',
+'Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta','W','Re','Os','Ir',
+'Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th','Pa','U','Np',
+'Pu','Am','Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg','Bh','Hs',
+'Mt','Ds','Rg','Cn',)
+
+
+#Convert all atomic symbols to lowercase
+
+atomic_symbols = [x.lower() for x in atomic_symbols]
+
+
+#Tuple containing the atomic masses corresponding to the
+#atomic symbols
+
+atomic_masses = (1.007944,4.0026022,6.9412,9.0121823,10.8117,12.01078,14.00672,
+15.99943,
+18.99840325,20.17976,22.989769282,24.30506,26.98153868,28.08553,
+30.9737622,32.0655,35.4532,39.9481,39.09831,40.0784,44.9559126,47.8671,
+50.94151,51.99616,54.9380455,55.8452,58.9331955,58.69344,63.5463,65.382,
+69.7231,72.641,74.921602,78.963,79.9041,83.7982,85.46783,87.621,88.905852,
+91.2242,92.906382,95.962,98,101.072,102.905502,106.421,107.86822,112.4118,
+114.8183,118.7107,121.7601,127.603,126.904473,131.2936,132.90545192,
+137.3277,138.905477,140.1161,140.907652,144.2423,145,150.362,151.9641,
+157.253,158.925352,162.5001,164.930322,167.2593,168.934212,173.0545,
+174.96681,178.492,180.947882,183.841,186.2071,190.233,192.2173,195.0849,
+196.9665694,200.592,204.38332,207.21,208.980401,209,210,222,223,226,227,
+232.038062,231.035882,238.028913,237,244,243,247,247,251,252,257,258,259,262,
+267,268,271,272,270,276,281,280,285,)
+
+
+#Create a dictionary linking atomic symbol with appropriate
+#mass
+
+data_table = dict(zip(atomic_symbols, atomic_masses))
+
+
+#Collects user input regarding the elements involved,
+#cleans up this input, and confirms that only letters,
+#and commas were entered
+
+def func_one():
+    elements = get_elements()
+    elements_stripped = rem_comm_spaces(elements)
+    alpha_only = only_alpha(elements_stripped)
+    while alpha_only == False:
+        print('\n')
+        print("I'm sorry, but you seem to have entered \
+a numerical value, invalid punctuation mark, or the empty string. \
+Please try again." + '\n')
+        elements = get_elements()
+        elements_stripped = rem_comm_spaces(elements)
+        alpha_only = only_alpha(elements_stripped)
+        continue
+    return elements
+
+
+#Formats a string containing only letters and commas
+#and determines if the user would like to quit the
+#program
+
+def func_two(elements):
+    elements_formatted = format_input(elements)
+    if elements_formatted == 'quit':
+        sys.exit()
+    else:
+        return elements_formatted
+
+
+#Determines which of the atomic symbols entered by
+#the user are legitimate atomic symbols and appends
+#the legitimate ones to a list called 'elements_list'
+
+def func_three(elements_formatted):
+    elements = elements_formatted.split(',')
+    elements_list = []
+    for element in elements:
+        if element in atomic_symbols:
+            elements_list.append(element)
+        else:
+            continue
+    return elements,elements_list
+
+
+#Compares the number of atomic symbols entered by 
+#the user with the subset of those that are actually 
+#valid
+
+def func_three_a(elements, elements_list):
+    if len(elements) == len(elements_list):
+        return True
+    else:
+        return False
+
+
+#In the case where the number of symbols entered by the user
+#is not the same as the number of valid symbols,
+#prompts the user to reenter his data
+
+def func_three_b(bool_legit,elements_list):
+    while bool_legit == False:
+        print("I'm sorry, but you have entered an invalid \
+atomic symbol. Please try again." + '\n')
+        elements = func_one()
+        elements_formatted = func_two(elements)
+        elements, elements_list = func_three(elements_formatted)
+        bool_legit = func_three_a(elements,elements_list)
+        continue
+        return elements, elements_list
+    return elements_list
+
+
+#Collects a list of comma-separated (numerical)
+#values from the user. If the values are not
+#actually whole numbers, this situation is
+#rectified
+
+def func_four():
+    numbers = get_numbers()
+    numbers_stripped = rem_comm_spaces(numbers)
+    num_only = only_num(numbers_stripped)
+    while num_only == False:
+        numbers = format_input(numbers)
+        if numbers == 'quit':
+            sys.exit()
+        else:
+            print("I'm sorry, but you seem to have entered \
+a letter, an invalid punctuation mark, or the empty string. \
+Please try again." + '\n')
+            numbers = get_numbers()
+            numbers_stripped = rem_comm_spaces(numbers)
+            num_only = only_num(numbers_stripped)
+            continue
+    return numbers
+
+
+#Converts each value in the string of whole number
+#values to an integer and appends each integer to the 
+#'numbers_list' list
+
+def func_five(numbers):
+    numbers_list = []
+    numbers = numbers.replace(' ','')
+    numbers = numbers.split(',')
+    for number in numbers:
+        numbers_list.append(int(number))
+
+    #print(numbers_list)
+    return numbers_list
+
+
+#Compares the list of elements to the list of
+#integers to determine whether or not the lists
+#are the same length. If not, the situation
+#is rectified
+
+def func_six(new_elements_list, numbers_list):
+    bool_value = comp_lists(new_elements_list, numbers_list)
+    while bool_value == False:
+        print("I'm sorry, but you have not entered the \
+same number of integers as atomic symbols. Please try again." + '\n')
+        elements = func_one()
+        elements_formatted = func_two(elements)
+        elements, elements_list = func_three(elements_formatted)
+        bool_legit = func_three_a(elements,elements_list)
+        numbers = func_four()
+        numbers_list = func_five(numbers)
+        bool_value = comp_lists(new_elements_list, numbers_list)
+        continue
+    return new_elements_list, numbers_list
+
+
+#Creates a dictionary by mapping each atomic symbol in
+#the elements list to the corresponding integer in
+#the numbers list
+
+def func_seven(new_elements_list, numbers_list):
+    print(new_elements_list)
+    print(numbers_list)
+    elements_dict = zip_lists(new_elements_list, numbers_list)
+    print(elements_dict)
+    return elements_dict
+
+
+#Calculates the molar mass of the molecule in question
+
+def func_eight(elements_dict, data_table):
+    molar_mass = calc_molar_mass(elements_dict, data_table)
+    return molar_mass
+
+
+#Asks the user which kind of calculation he would like to
+#perform and confirms that the input is non-numerical,
+
+def func_nine():
+    calc = what_calc()
+    calc_cleaned = clean_what_calc(calc)
+    alpha_bool_value = is_alpha(calc_cleaned)
+    while alpha_bool_value == False:
+        print("I'm sorry, but you have entered a number, \
+a punctuation mark, or the empty string. Please try again" + '\n')
+        calc = what_calc()
+        calc_cleaned = clean_what_calc(calc)
+        alpha_bool_value = is_alpha(calc_cleaned)
+        continue
+        return calc_cleaned
+    return calc_cleaned
+
+
+#Determines whether or not the user would like to
+#eit the program and acts accordingly
+
+def func_nine_a(calc_cleaned):
+    if calc_cleaned == 'quit':
+        sys.exit()
+    else:
+        return calc_cleaned
+
+
+#Converts the calculation type string to lowercase,
+#confirms that the string is only one character in length,
+#and rectifies the situation if this is not the case
+
+def func_ten(calc_cleaned):
+    calc_cleaned = calc_cleaned.lower()
+    len_bool_value = check_len_what_calc(calc_cleaned)
+    while len_bool_value == False:
+        print("I'm sorry, but you have entered a non-legitimate \
+calculation type. Please try again." + '\n')
+        calc_cleaned = func_nine()
+        calc_cleaned = calc_cleaned.lower()
+        len_bool_value = check_len_what_calc(calc_cleaned)
+        continue
+        return calc_cleaned
+    return calc_cleaned
+
+
+#Asks the user to reenter the calculation type if
+#he has not entered either 'g' or 'm'
+
+def func_ten_a(calc_cleaned):
+    while calc_cleaned not in ['g','m']:
+        calc_cleaned = func_nine()
+        calc_cleaned = func_nine_a(calc_cleaned)
+        calc_cleaned = func_ten(calc_cleaned)
+        continue
+    return calc_cleaned
+
+
+#Determines which type of calculation the user
+#would like to perform and then collects and
+#formats the corresponding data. Confirms that
+#the user enters floating point numbers
+#for his data, and handles situations
+#in which floating point values were not
+#entered. Also performs the desired calculation
+#and outputs the answer to standard out
+
+def func_eleven(calc_cleaned, molar_mass):
+    calc = analyze_what_calc(calc_cleaned)
+    if calc == 'g':
+        moles = how_many_moles()
+        formatted_moles = format_moles(moles)
+        float_bool_value = confirm_float_moles(formatted_moles)
+        while float_bool_value == False:
+            bool_not_float = handle_not_float(formatted_moles)
+            if bool_not_float == True:
+                sys.exit()
+
+            else:
+                print("I'm sorry, but you did not enter a floating-point \
+value. Please try again." + '\n')
+                moles = how_many_moles()
+                formatted_moles = format_moles(moles)
+                float_bool_value = confirm_float_moles(formatted_moles)
+                continue
+        moles = make_moles_float(moles)
+        grams = moles_to_grams(moles,molar_mass)
+        answer = str(grams) + ' ' + 'g'
+        print('\n')
+        print("You have " + answer + '\n')
+
+    elif calc == 'm':
+        grams = how_many_grams()
+        formatted_grams = format_grams(grams)
+        float_bool_value = confirm_float_grams(formatted_grams)
+        while float_bool_value == False:
+            bool_not_float = handle_not_float(formatted_grams)
+            if bool_not_float == True:
+                sys.exit()
+
+            else:
+                print("I'm sorry, but you did not enter a floating-point \
+value. Please try again." + '\n')
+                grams = how_many_grams()
+                formatted_grams= format_grams(grams)
+                float_bool_value = confirm_float_grams(formatted_grams)
+                continue
+        grams = make_grams_float(grams)
+        moles = grams_to_moles(grams,molar_mass)
+        answer = str(moles) + ' ' + 'mol'
+        print('\n') 
+        print("You have " + answer + '\n')
+
+
+#Asks the user if he would like to perform another calculation on 
+#the same molecule or quit the program. Handles input as the 
+#preceeding functions did.
+
+def func_twelve():
+    another_calc_dec = another_calc()
+    cleaned_calc_dec  = rem_comm_spaces(another_calc_dec)
+    alpha_bool_value = is_alpha(cleaned_calc_dec)
+    while alpha_bool_value == False:
+        print("I'm sorry, but I did not understand that. \
+Please try again. " + '\n')
+        another_calc_dec = another_calc()
+        cleaned_calc_dec = rem_comm_spaces(another_calc_dec)
+        alpha_bool_value = is_alpha(cleaned_calc_dec)
+        continue
+    return another_calc_dec
+    
+
+#Determines if the user desires to quit or perform another calculation
+#and acts accordingly
+
+def func_thirteen(another_calc_dec,molar_mass):
+    form_another_calc = format_input(another_calc_dec)
+    while form_another_calc not in ['quit','yes']:
+        print("I'm sorry, but I did not understand that. \
+Please try again. " + '\n')
+        another_calc_dec = func_twelve()
+        form_another_calc = format_input(another_calc_dec)
+        continue
+
+    if form_another_calc == 'quit':
+        sys.exit()
+    
+    else:
+       calc_cleaned = func_nine()
+       calc_cleaned = func_nine_a(calc_cleaned)
+       calc_cleaned = func_ten(calc_cleaned)
+       calc_cleaned = func_ten_a(calc_cleaned)
+       answer = func_eleven(calc_cleaned, molar_mass)
+
+    another_calc_dec = func_twelve()
+    func_thirteen(another_calc_dec,molar_mass)
+
+
+
+
+
+def main():
+    elements = func_one()
+    elements_formatted = func_two(elements)
+    elements,elements_list = func_three(elements_formatted)
+    bool_legit = func_three_a(elements,elements_list)
+    new_elements_list = func_three_b(bool_legit,elements_list)
+    numbers = func_four()
+    numbers_list = func_five(numbers)
+    new_elements_list, numbers_list = func_six(new_elements_list, numbers_list)
+    elements_dict = func_seven(new_elements_list, numbers_list)
+    molar_mass = func_eight(elements_dict, data_table)
+    calc_cleaned = func_nine()
+    calc_cleaned = func_nine_a(calc_cleaned)
+    calc_cleaned = func_ten(calc_cleaned)
+    calc_cleaned = func_ten_a(calc_cleaned)
+    answer = func_eleven(calc_cleaned,molar_mass)
+    another_calc_dec = func_twelve()
+    func_thirteen(another_calc_dec,molar_mass)
+    
+
+
+
+if __name__=='__main__':
+    main()
